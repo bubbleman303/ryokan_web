a3ec334d90197e111cecf295e6638de92c05a542
keizaburo.takashiba@airitech.co.jp <keizaburo.takashiba@airitech.co.jp>
2021-09-09 00:27:51 +0900

    100 kaneko

--- ryokan/ryokan_app/views.py
+++ ryokan/ryokan_app/views.py
@@ -3,9 +3,11 @@ from django.shortcuts import render
 from django.shortcuts import redirect
 from django.views.generic import TemplateView
 
+
 class TopView(TemplateView):
     template_name = "index.html"
 
     def get(self, request, *args, **kwargs):
-        context = {}
+        x = 100 ** 100
+        context = {"x": x}
         return render(self.request, self.template_name, context)
